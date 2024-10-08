5130bb1603e6805e6a0275aaf57d85be22002ec6
Ale2l <149272755+Ale2l@users.noreply.github.com>
2024-10-08 15:31:33 -0500

    Update View_Login.java
    le borré  una línea de descripcion

--- ON_LINE/src/view/View_Login.java
+++ ON_LINE/src/view/View_Login.java
@@ -4,7 +4,7 @@
  * Fundación Universitaria Juan de Castellanos
  * Asignatura: Programación Orientada a Objetos
  * Descripción: Esta clase principal se utiliza para iniciar la aplicación, 
- * creando instancias del modelo, la vista y el controlador, y ejecutando el programa.
+ * 
  * Fecha: 26 - 08 - 2024
  * Autor: Maycol Saavedra Saiz
  * 
